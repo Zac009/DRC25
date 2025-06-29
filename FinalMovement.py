90f2d619ba3274284053d43c5ad39407bc281af6
Zac0009 <103079687+Zac009@users.noreply.github.com>
2025-06-29 19:32:47 +1000

    Update FinalMovement.py

--- FinalMovement.py
+++ FinalMovement.py
@@ -162,7 +162,7 @@ class Vision:
                 blue_mean = np.mean(blue_coords, axis=0)[0]
                 print("No yellow_coords found!!!")
                 #midpoint_x = int(blue_mean[0]) - (self.width // 4)
-                midpoint_x = int(blue_mean[0]) - 300
+                midpoint_x = int(blue_mean[0]) - 50
                 midpoint_y = 180
                 midpoint = (midpoint_x, midpoint_y)
                 self.center_points.append(midpoint)
@@ -171,7 +171,7 @@ class Vision:
             elif yellow_coords is not None:
                 yellow_mean = np.mean(yellow_coords, axis=0)[0]
                 print("No blue_coords found!!!")
-                midpoint_x = int(yellow_mean[0]) + 300
+                midpoint_x = int(yellow_mean[0]) + 50
                 midpoint_y = 180
                 midpoint = (midpoint_x, midpoint_y)
                 self.center_points.append(midpoint)
