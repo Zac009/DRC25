a6b7736b954a04c4e14ec804885c527d93eabdb3
Zac0009 <103079687+Zac009@users.noreply.github.com>
2025-07-02 12:26:48 +1000

    Update BlueFinal3.py

--- BlueFinal3.py
+++ BlueFinal3.py
@@ -266,7 +266,7 @@ class Vision:
                                 print("Corner")
                                 self.drive(DRIVE_CORNER)
                                 self.state = "CORNER"
-                            time.sleep(0.3)
+                            time.sleep(0.1)
                             self.drive(DRIVE_STOP)
                         else:
                             if self.state == "FORWARRRD":
@@ -275,7 +275,7 @@ class Vision:
                                 print("Forward")
                                 self.drive(DRIVE_FORWARD)
                                 self.state = "FORWARD"
-                            time.sleep(0.3)
+                            time.sleep(0.1)
                             self.drive(DRIVE_STOP)
                 except Exception as e:
                     print(f"There was an error: {e}")
