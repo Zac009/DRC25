0affbb9d9d0d316554392e61f617659158882c5a
Zac0009 <103079687+Zac009@users.noreply.github.com>
2025-07-03 10:12:34 +1000

    Update BLUEEEE.py

--- BLUEEEE.py
+++ BLUEEEE.py
@@ -267,6 +267,8 @@ class Vision:
                                 self.drive(DRIVE_CORNER)
                                 self.state = "CORNER"
                             time.sleep(0.1)
+                            self.drive(DRIVE_STOP)
+                            time.sleep(1)
                         else:
                             if self.state == "FORWARRRD":
                                 pass
@@ -275,6 +277,8 @@ class Vision:
                                 self.drive(DRIVE_FORWARD)
                                 self.state = "FORWARD"
                             time.sleep(0.1)
+                            self.drive(DRIVE_STOP)
+                            time.sleep(1)
                 except Exception as e:
                     print(f"There was an error: {e}")
                     self.drive(DRIVE_STOP)
