f170370f90ecefe98434f89440ece13e774385bd
Zac0009 <103079687+Zac009@users.noreply.github.com>
2025-07-03 11:13:00 +1000

    Update bounce.py

--- bounce.py
+++ bounce.py
@@ -125,7 +125,7 @@ class Vision:
                         elif offset > 0:
                             self.steer(STEER_RIGHT)
                             self.drive(DRIVE_CORNER)
-                            time.sleep(0.5)
+                            time.sleep(0.4)
                             self.last_steer = STEER_RIGHT
                             self.last_drive = DRIVE_CORNER
                             self.drive(DRIVE_STOP)
@@ -133,7 +133,7 @@ class Vision:
                         else:
                             self.steer(STEER_LEFT)
                             self.drive(DRIVE_CORNER)
-                            time.sleep(0.5)
+                            time.sleep(0.4)
                             self.last_steer = STEER_LEFT
                             self.last_drive = DRIVE_CORNER
                             self.drive(DRIVE_STOP)
@@ -142,7 +142,7 @@ class Vision:
                         print("Blue")
                         self.steer(STEER_LEFT)
                         self.drive(DRIVE_CORNER)
-                        time.sleep(0.5)
+                        time.sleep(0.4)
                         self.last_steer = STEER_LEFT
                         self.last_drive = DRIVE_CORNER
                         self.drive(DRIVE_STOP)
@@ -151,7 +151,7 @@ class Vision:
                         print("Yellow")
                         self.steer(STEER_RIGHT)
                         self.drive(DRIVE_CORNER)
-                        time.sleep(.5)
+                        time.sleep(0.4)
                         self.last_steer = STEER_RIGHT
                         self.last_drive = DRIVE_CORNER
                         self.drive(DRIVE_STOP)
@@ -161,7 +161,7 @@ class Vision:
                         # No lines seen, continue last command
                         self.steer(self.last_steer)
                         self.drive(self.last_drive)
-                        time.sleep(1)
+                        time.sleep(0.5)
                         self.drive(DRIVE_STOP)
                         time.sleep(0.2)
                 except Exception as e:
