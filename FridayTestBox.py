7241ebff99560454b2a088b0441dd27bcff83ff9
Zac0009 <103079687+Zac009@users.noreply.github.com>
2025-07-04 11:20:44 +1000

    Update FridayTestBox.py

--- FridayTestBox.py
+++ FridayTestBox.py
@@ -152,14 +152,14 @@ class Vision:
                                 if purple_x < self.width // 2:
                                     # Purple box on left, steer right
                                     print("Purple box ahead on left, steering RIGHT")
-                                    self.steer(1750)
+                                    self.steer(1900)
                                     self.drive(DRIVE_CORNER)
                                     self.last_steer = STEER_RIGHT
                                     self.last_drive = DRIVE_CORNER
                                 else:
                                     # Purple box on right, steer left
                                     print("Purple box ahead on right, steering LEFT")
-                                    self.steer(1250)
+                                    self.steer(1100)
                                     self.drive(DRIVE_CORNER)
                                     self.last_steer = STEER_LEFT
                                     self.last_drive = DRIVE_CORNER
