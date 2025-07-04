b0318d7223fc241a1dfdd25b88698d82dee6a28b
Zac0009 <103079687+Zac009@users.noreply.github.com>
2025-07-04 14:27:45 +1000

    Update FridayTest1.py

--- FridayTest1.py
+++ FridayTest1.py
@@ -14,8 +14,8 @@ STEER_CENTER = 1500
 STEER_RIGHT = 2000
 
 DRIVE_STOP = 1500
-DRIVE_FORWARD = 1670
-DRIVE_CORNER = 1640
+DRIVE_FORWARD = 1680
+DRIVE_CORNER = 1650
 DRIVE_BACKWARD = 1400
 
 DIST_FROM = 150
@@ -66,8 +66,6 @@ class Vision:
     def main(self):
         self.last_steer = STEER_CENTER
         self.last_drive = DRIVE_FORWARD
-        self.drive(DRIVE_FORWARD)
-        time.sleep(0.5)
         try:
             while True:
                 ret, self.frame = self.cap.read()
